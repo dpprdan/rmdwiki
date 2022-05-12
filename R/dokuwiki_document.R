4bc78deb803d361701f27ca2c3306991ce4d8601
Athanasia Monika Mowinckel <Athanasiamo@users.noreply.github.com>
2022-05-12 09:52:05 +0200

    add dokuwiki (#2)

---
+++ R/dokuwiki_document.R
@@ -0,0 +1,18 @@
+#' Convert to DokuWiki Markup
+#'
+#' Format for converting from R Markdown to DokuWiki
+#'
+#' @inheritParams mediawiki_document
+#'
+#' @seealso [DokuWiki reference manual](https://www.dokuwiki.org/dokuwiki)
+#'
+#' @inherit mediawiki_document return
+#' @export
+docuwiki_document <- function(keep_md = FALSE, pandoc_args = NULL) {
+  rmarkdown::output_format(
+    knitr = rmarkdown::knitr_options(opts_chunk = list(dev = "png")),
+    pandoc = rmarkdown::pandoc_options(to = "dokuwiki", args = pandoc_args),
+    keep_md = keep_md,
+    clean_supporting = TRUE
+  )
+}
